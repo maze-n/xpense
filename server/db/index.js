a5455be3b0019e5256c95dbc076bdb8d64fb83b7
Mazen Maliyakkal <Mazen.Maliyakkal@Orioninc.com>
2024-04-02 17:41:30 +0530

    Adds .env files

--- server/db/index.js
+++ server/db/index.js
@@ -1,29 +1,31 @@
 import { CosmosClient } from "@azure/cosmos";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: "./server/.env" });
 
 export const cosmos = new CosmosClient({
-    endpoint: 'https://127.0.0.1:8081/',
-    key: 'C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=='
-})
+  endpoint: process.env.DATABASE_URL,
+  key: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+});
 
 async function initializeDB() {
-    const { database } = await cosmos.databases.createIfNotExists({
-        id: 'xpense',
-        throughput: 400
-    })
+  const { database } = await cosmos.databases.createIfNotExists({
+    id: "xpense",
+    throughput: 400,
+  });
 
-    const { container: expenseContainer } = await database.containers.createIfNotExists({
-        id: 'expenses',
-        partitionKey: {
-            paths: [
-                '/id',
-            ]
-        }
-    })
+  const { container: expenseContainer } =
+    await database.containers.createIfNotExists({
+      id: "expenses",
+      partitionKey: {
+        paths: ["/id"],
+      },
+    });
 
-    return {
-        database,
-        expenseContainer
-    }
+  return {
+    database,
+    expenseContainer,
+  };
 }
 
-export { initializeDB }
+export { initializeDB };
--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import appRouter from './routes/index.js';
-import { initializeDB } from './db/index.js';
+import express from "express";
+import cors from "cors";
+import appRouter from "./routes/index.js";
+import { initializeDB } from "./db/index.js";
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 const PORT = 8000;
@@ -11,17 +11,19 @@ app.use(express.json());
 
 app.use(appRouter);
 
-initializeDB().then(() => {
+initializeDB()
+  .then(() => {
     app.listen(PORT, function (err) {
-        if (err) {
-            console.log("Failed to start the server!");
-            console.error(err);
-            return;
-        }
+      if (err) {
+        console.log("Failed to start the server!");
+        console.error(err);
+        return;
+      }
 
-        console.log("Server running on Port:", PORT);
-    })
-}).catch((err) => {
+      console.log("Server running on Port:", PORT);
+    });
+  })
+  .catch((err) => {
     console.log("Failed to connect with the database!");
     console.error(err);
-})
+  });
